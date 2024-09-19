12cfc1871331e392a46c2f2a7db7215782d20d00
Natã <natadiassantos@ucl.br>
2024-09-18 21:34:28 -0300

    dev-natan-restruturacao

--- FrontEnd/src/componentes/paginaupload.js
+++ FrontEnd/src/componentes/paginaupload.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react';
 import '../estilos/paginaupload.css';
 
-import { Icon } from '@iconify/react';
-
-import IconeImagem from '@iconify-icons/ph/image-light';
-import IconeVideo from '@iconify/icons-mdi/video-outline';
-import IconeTexto from '@iconify/icons-fluent/text-t-12-filled';
-
 import ModalImagem from './modais/modal-imagem.js';
 import ModalVideo from './modais/modal-video.js';
 import ModalTexto from './modais/modal-texto.js';
@@ -34,21 +28,18 @@ const PaginaUpload = () => {
 
         <div className='column1'>
           <div className='imagem'>
-            <Icon icon={IconeImagem} className="imagem-icone" />
             <button className="botao-imagem" onClick={abrirModalImagem}>arquivo</button>
 
             {modalImagemAberto && <ModalImagem fecharModal={fecharModalImagem} />}
           </div>
 
           <div className='video'>
-            <Icon icon={IconeVideo} className="video-icone" />
             <button className="botao-video" onClick={abrirModalVideo}>arquivo</button>
 
             {modalVideoAberto && <ModalVideo fecharModal={fecharModalVideo} />}
           </div>
 
           <div className='texto'>
-            <Icon icon={IconeTexto} className="texto-icone" />
             <button className="botao-texto" onClick={abrirModalTexto}>arquivo</button>
 
             {modalTextoAberto && <ModalTexto fecharModal={fecharModalTexto} />}
