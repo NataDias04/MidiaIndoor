130e43bb9676b573a9d4a4a8eb9407811c4a9e68
Natã <natadiassantos@ucl.br>
2024-09-27 06:06:02 -0300

    dev-natan-front-end-editar-playlist

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PaginaUpload from './componentes/paginaupload.js';
 import PaginaLayout from './componentes/paginalayout.js';
 import PaginaLogin from './componentes/paginalogin.js';
-import PaginaPlaylist from './componentes/paginaplaylist.js';
+import PaginaPlaylistLayout1 from './componentes/paginaplaylistlayout1.js';
+import PaginaPlaylistLayout2 from './componentes/paginaplaylistlayout2.js';
+import PaginaPlaylistLayout3 from './componentes/paginaplaylistlayout3.js';
 import PaginaCentral from './componentes/paginacentral.js';
 import PaginaVerPlaylist from './componentes/paginaverplaylist.js';
 
 // <PaginaLayout />
 // <PaginaUpload />
 // <PaginaLogin/>
-// <PaginaPlaylist/>
+// <Paginaplaylistlayout1/>
+// <Paginaplaylistlayout2/>
+// <PaginaPlaylistLayout3/>
 // <PaginaCentral/>
 // <PaginaVerPlaylist/>
 
@@ -25,8 +29,10 @@ const App = () => {
           <Route path="/central" element={<PaginaCentral/>} /> {/* Página de central */}
           <Route path="/upload" element={<PaginaUpload />} /> {/* Página de upload */}
           <Route path="/layout" element={<PaginaLayout />} /> {/* Página de layout */}
-          <Route path="/login" element={<PaginaLogin />} /> {/* Página de login */}
-          <Route path="/playlist" element={<PaginaPlaylist />} /> {/* Página de playlist */}
+          <Route path="/login" element={<PaginaLogin />} /> {/* Página de login */}  
+          <Route path="/playlistlayout1" element={<PaginaPlaylistLayout1 />} /> {/* Página de playlist */}
+          <Route path="/playlistlayout2" element={<PaginaPlaylistLayout2 />} />
+          <Route path="/playlistlayout3" element={<PaginaPlaylistLayout3 />} />
           <Route path="/verplaylist" element={<PaginaVerPlaylist/>} />
         </Routes>
 
