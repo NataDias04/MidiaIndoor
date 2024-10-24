753e79b29d44bf52335755f6a5d72369d6601a11
Natã <natadiassantos@ucl.br>
2024-10-24 03:18:05 -0300

    dev

--- FrontEnd-PlaylistPlayer/src/componentes/players/player1.js
+++ FrontEnd-PlaylistPlayer/src/componentes/players/player1.js
@@ -14,39 +14,42 @@ const Player1 = () => {
     const location = useLocation();
     const { PlaylistSelecionada } = location.state || {};
 
-    const BuscarMidias = async () => {
-        try {
-          const resultados = await Promise.all(
-            PlaylistSelecionada.map(async (_id) => {
-              const funcoesDeBusca = [
-                buscarVideo,
-                buscarVideoLink,
-                buscarHtml,
-                buscarTextoSimples,
-                buscarImagem,
-                buscarImagemLink,
-              ];
-
-              for (const func of funcoesDeBusca) {
-                const resultado = await func(_id);
-                if (resultado) {
-                  return { _id, tipo: func.name, resultado };
-                }
-              }
-              return { _id, tipo: 'nenhum', resultado: null };
-            })
-          );
+    const BuscarMidia = async (midia) => {
+      const funcoes = [
+        buscarVideo,
+        buscarVideoLink,
+        buscarHtml,
+        buscarTextoSimples,
+        buscarImagem,
+        buscarImagemLink
+      ];
     
-          console.log('Resultados:', resultados);
+      for (const func of funcoes) {
+        try {
+          const resultado = await func(midia);
 
+          if (resultado) {
+            console.log(`Função ${func.name} retornou:`, resultado);
+            return resultado;
+          }
         } catch (erro) {
-          console.error('Erro ao buscar mídias:', erro);
+          console.error(`Erro ao executar ${func.name}:`, erro);
         }
+      }
+    
+      console.log('Nenhuma mídia encontrada para esta playlist.');
+      return null;
       };
+
+    const CarregarMidia = async (playlist) => {
+      for (const midia of playlist.ordemMidias) {
+        console.log(BuscarMidia(midia));
+      }
+    };
 
   return (
     <div>
-        {console.log(BuscarMidias(PlaylistSelecionada))}
+
     </div>
   );
 };
